2bc21301241cbb83ae4aaf75c0af8ae9f6871664
CJ Miller <cjmiller721@gmail.com>
2017-01-29 18:29:15 -0500

    Inital Commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,8 @@
+/**
+ * Created by CJ on 1/29/2017.
+ */
+public class Library {
+    public static void main(String[] args) {
+        System.out.println("This is a library");
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by CJ on 1/29/2017.
+ */
+public class LibraryTest {
+
+    @Test
+    public void alwaysPass() {
+        assertEquals(true,true);
+    }
+}
