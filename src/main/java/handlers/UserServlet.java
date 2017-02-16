d59844ff22d6af17ced4f0f032b9562ef3acb319
CJ Miller <cjmiller721@gmail.com>
2017-02-16 17:12:05 -0500

    Change to specify each one

--- src/main/java/handlers/UserServlet.java
+++ src/main/java/handlers/UserServlet.java
@@ -167,7 +167,7 @@ public class UserServlet implements IServlet {
 
         HashMap headers = new HashMap<String, String>();
         headers.put("Access-Control-Allow-Origin", "*");
-        headers.put("Access-Control-Allow-Methods", "*");
+        headers.put("Access-Control-Allow-Methods", "HEAD, GET, DELETE, POST, PUT");
         responseBuilder.setHeaders(headers);
         responseBuilder.setConnection(Protocol.CLOSE);
         responseBuilder.setBody(body);
